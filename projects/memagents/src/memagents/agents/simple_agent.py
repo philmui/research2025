0dc50ce4558134436fee01ca2c7199bd1de87ba8
Phil Mui <pmui@salesforce.com>
2025-06-21 18:13:44 -0700

    Updated with 2 types of long term memories

--- projects/memagents/src/memagents/agents/simple_agent.py
+++ projects/memagents/src/memagents/agents/simple_agent.py
@@ -5,7 +5,9 @@ import argparse
 import asyncio
 from datetime import datetime
 from llama_index.core.agent.workflow import FunctionAgent
-from llama_index.core.memory import Memory
+from llama_index.core.memory import (
+    Memory, StaticMemoryBlock, FactExtractionMemoryBlock, VectorMemoryBlock, InsertMethod
+)
 from llama_index.llms.openai import OpenAI
 from llama_index.core.tools import FunctionTool
 from llama_index.core.workflow import Context
@@ -20,11 +22,31 @@ def get_current_weather(city: str):
 
 tools = [get_current_time, get_current_weather]
 
+static_memory_block = StaticMemoryBlock(
+    name="core_info",
+    static_content="My name is MemAgent, and I live in San Francisco.  I am here to help ASDRP researhers",
+    priority=0,
+)
+
+fact_extraction_memory_block = FactExtractionMemoryBlock(
+    name="extracted_info",
+    llm=llm,
+    max_facts=50,
+    priority=1,
+)
+
+long_term_memory_blocks = [
+    static_memory_block,
+    fact_extraction_memory_block,
+]
+
 memory = Memory.from_defaults(
     session_id="simple_agent",
     token_limit=10000,
     chat_history_token_ratio=0.7,
-    token_flush_size=2000
+    token_flush_size=2000,
+    memory_blocks=long_term_memory_blocks,
+    insert_method=InsertMethod.SYSTEM
 )
 
 agent = FunctionAgent(
