fc69fc5df864d4f6c820578879eeb1312cfb05b5
Phil Mui <thephilmui@gmail.com>
2025-11-02 17:28:26 -0800

    Adding Langchain agent in MMLU evaluation

--- 13-langchain/asdrp/agents/lang_hello.py
+++ 13-langchain/asdrp/agents/lang_hello.py
@@ -6,16 +6,25 @@ _ = load_dotenv(find_dotenv())
 
 def say_hello(name: str) -> str:
     """Greet the person `name`."""
+    print(f"==> Greeting {name}...")
     return f"It's nice to meet you, {name}!"
 
+# Creating a "System 1" reasoning agent
 agent = create_agent(
     model="openai:gpt-5-mini",
     tools=[say_hello],
     system_prompt="You are a helpful assistant",
 )
 
-response = agent.invoke(
-    {"messages": [{"role": "user", "content": "Hi, I am Joe."}]}
-)
-
-print(response["messages"][-1].content)
+async def main():
+    
+    user_input = input("How can I help you? ")
+    while user_input != "":
+        response = agent.invoke(
+            {"messages": [{"role": "user", "content": user_input}]}
+        )
+        print(response["messages"][-1].content)
+        user_input = input("How can I help you? ")
+    
+if __name__ == "__main__":
+    asyncio.run(main())
